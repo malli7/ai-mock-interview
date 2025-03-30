44ebc96499cfebfe59f2b71d0c2a9d3bdd4077ca
Malli7 <mallireddy2001@gmail.com>
2025-03-30 10:50:24 -0500

    ignore erors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Fail build if there are ESLint errors
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // Fail build if there are TypeScript errors
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
